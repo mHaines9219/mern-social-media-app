d96f3f5c90efa8dd89a863811270d588a2835f92
mHaines331 <mhaines9219@gmail.com>
2023-11-24 19:36:47 -0800

    build model

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose   from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
-import helmet from "helmet";
+import helmet, { originAgentCluster } from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import { register } from "module";
+import { register } from "./controller/auth.js";
 /*CONFIGURATIONS*/
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,4 +18,38 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy);
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); //setting up local storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/tmp/my-uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage });
+
+//ROUTES WITH FILES
+
+app.post("/auth/register", upload.single("picture"), register);
+
+//MONGOOSE SETUP
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  });
